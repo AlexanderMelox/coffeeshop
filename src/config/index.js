782e5f2ee71b35fda2557e05300bd1ba8a52425f
Alexander Melo <alexander.melo@ibm.com>
2019-09-16 14:05:21 -0500

    first commit

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+export default {
+  PORT: 3000,
+  DB_LOCAL: 'mongodb://localhost:27017/coffee-shop',
+  DB_HOSTED:
+    'mongodb+srv://fedibm:fedibm@cluster0-abecu.mongodb.net/coffeeshop?retryWrites=true&w=majority'
+};
