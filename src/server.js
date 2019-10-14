91e0ff224c6d7cc2acec7601b5e912b966553174
Alexander Melo <alexander.melo@ibm.com>
2019-10-14 11:24:57 -0500

    actually start here

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 // filesystem package to read / write files
 const { ApolloServer, gql } = require('apollo-server');
 
+// Utils for querying the "database"
 const {
   getAllOrders,
   getOrderByID,
@@ -10,92 +11,10 @@ const {
 } = require('./utils/orders');
 
 // Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  enum CoffeeSize {
-    SHORT
-    TALL
-    GRANDE
-    MUYGRANDE
-  }
-
-  enum CoffeeFlavor {
-    MOCHA
-    ALMOND
-    HAZELNUT
-    VANILLA
-  }
-
-  type Order {
-    id: ID!
-    name: String!
-    email: String!
-    size: CoffeeSize!
-    espressoShots: Int
-    iced: Boolean
-    flavor: CoffeeFlavor
-  }
-
-  input NewOrderInput {
-    name: String!
-    email: String!
-    size: CoffeeSize!
-    espressoShots: Int
-    iced: Boolean
-    flavor: CoffeeFlavor
-  }
-
-  input UpdateOrderInput {
-    name: String
-    email: String
-    size: CoffeeSize
-    espressoShots: Int
-    iced: Boolean
-    flavor: CoffeeFlavor
-  }
-
-  type Query {
-    orders: [Order]!
-    order(id: ID!): Order!
-  }
-
-  type Mutation {
-    createOrder(input: NewOrderInput!): Order!
-    updateOrder(id: ID!, input: UpdateOrderInput!): Order!
-    deleteOrder(id: ID!): Order
-  }
-
-  schema {
-    query: Query
-    mutation: Mutation
-  }
-`;
+const typeDefs = gql``;
 
 // Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    orders: () => {
-      return getAllOrders();
-    },
-    order: (obj, args) => {
-      const order = getOrderByID(args.id);
-      return order;
-    }
-  },
-  Mutation: {
-    createOrder(obj, { input }) {
-      const newOrder = insertOne(input);
-      return newOrder;
-    },
-    updateOrder(obj, { id, input }) {
-      let updatedOrder = updateOrderByID(id, input);
-      return updatedOrder;
-    },
-    deleteOrder(obj, { id }) {
-      const deletedOrder = deleteOrderByID(id);
-      return deletedOrder;
-    }
-  }
-};
+const resolvers = {};
 
 const server = new ApolloServer({
   typeDefs,
